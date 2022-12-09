78b78ef7ff8d2d3a21267c5d923b41ce2cba32a7
ROSMI1995 <rosmithomas212@gmail.com>
2022-12-09 15:51:51 +0530

    second commot

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Task(models.Model):
-	task_head = models.CharField(max_length=255)
+	title = models.CharField(max_length=255)
 	description = models.TextField()
+	date = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-		return str(self.task_head)
+		return str(self.title)
 
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import TodoForm
+from .models import Task
 
 # Create your views here.
 def home(request):
-	template = loader.get_template('index.html')
-	return HttpResponse(template.render())
+	item_list = Task.objects.order_by("-date")
+	if request.method == "POST":
+		form = TodoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('todo')
+
+	form = TodoForm()
+
+	page = {
+			"forms" : form,
+			"list" : item_list,
+			"title" : "TODO LIST",
+
+	}
+	return render(request, 'index.html', page)
+# function to remove tasks
+
+def remove(request, id):
+	item=Task.objects.get(id=id)
+	item.delete()
+	messages.info(request, "Task Removed !!!")
+	return redirect ('todo')
